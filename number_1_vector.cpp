e01985cdb862d59f17e4e38ebf703eba31ca907c
Elizer Dolorosa <43069251+SuchWowEl@users.noreply.github.com>
2023-06-30 01:57:32 +0800

    like number_1 but using vector

---
+++ number_1_vector.cpp
@@ -0,0 +1,189 @@
+/* MEMBERS:
+    Guingao, Rhea Salve
+    Dolorosa, Elizer
+    Aclo, Romeo Giullano
+*/
+
+#include <iostream>
+#include <limits>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int numberOfEdges(vector<vector<int>> graph)
+{
+    int count = 0;
+    for (int i = 0; i < graph.size(); i++)
+        for (int j = 0; j < graph[i].size(); j++)
+            if (graph[i][j] > 0)
+                count++;
+    return count;
+}
+
+int numberOfEdges(vector<vector<bool>> graph)
+{
+    int count = 0;
+    for (int i = 0; i < graph.size(); i++)
+        for (int j = 0; j < graph[i].size(); j++)
+            if (graph[i][j] > 0)
+                count++;
+    return count;
+}
+
+void dijkstraShortestPaths(vector<vector<int>> graph, int startVertex)
+{
+    int dist[graph.size()];
+    int prev[graph.size()];
+    bool visited[graph.size()];
+
+    for (int i = 0; i < graph.size(); i++)
+    {
+        dist[i] = numeric_limits<int>::max();
+        prev[i] = -1;
+        visited[i] = false;
+    }
+
+    dist[startVertex] = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, startVertex});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        int uDist = pq.top().first;
+        pq.pop();
+
+        if (uDist > dist[u])
+        {
+            continue;
+        }
+
+        visited[u] = true;
+
+        for (int v = 0; v < graph.size(); v++)
+        {
+            int temp = graph[u][v];
+            if (temp > 0)
+            {
+                int weight = temp;
+                int newDist = dist[u] + weight;
+                if (newDist < dist[v])
+                {
+                    dist[v] = newDist;
+                    prev[v] = u;
+                    if (!visited[v])
+                    {
+                        pq.push({newDist, v});
+                    }
+                }
+            }
+        }
+    }
+
+    cout << "Shortest paths from vertex " << 1 << ":\n";
+    for (int v = 0; v < graph.size(); v++)
+    {
+        if (v != startVertex)
+        {
+            cout << "Vertex " << v + 1 << ": ";
+            if (dist[v] == numeric_limits<int>::max())
+            {
+                cout << "No path found.";
+            }
+            else
+            {
+                int path[graph.size()];
+                int pathLength = 0;
+                int currVertex = v;
+                while (currVertex != -1)
+                {
+                    path[pathLength++] = currVertex;
+                    currVertex = prev[currVertex];
+                }
+                for (int i = pathLength - 1; i >= 0; i--)
+                {
+                    cout << path[i] + 1;
+                    if (i > 0)
+                    {
+                        cout << " -> ";
+                    }
+                }
+                cout << " (length = " << dist[v] << ")";
+            }
+            cout << endl;
+        }
+    }
+}
+
+bool isGraphConnected(vector<vector<int>> matrix, int startVertex)
+{
+    startVertex += 1;
+    bool visited[matrix.size()] = {false};
+
+    queue<int> q;
+    q.push(startVertex);
+    visited[startVertex] = true;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        for (int v = 0; v < matrix.size(); v++)
+        {
+            if (matrix[u][v] > 0 && !visited[v])
+            {
+                q.push(v);
+                visited[v] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        if (!visited[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main()
+{
+    vector<vector<int>> graph = {
+        {0, 2, 0, 20, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {3, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 8, 0, 0, 6, 4, 0, 0, 0},
+        {0, 0, 7, 0, 0, 0, 0, 3, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 2, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}};
+
+    vector<vector<bool>> booleanGraph = {
+        {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 1, 0}};
+
+    int numEdges = numberOfEdges(booleanGraph);
+    cout << "The number of edges in the graph is " << numEdges << ".\n \n";
+
+    int startVertex = 0;
+    dijkstraShortestPaths(graph, startVertex);
+
+    cout << "\nIs the graph connected? " << boolalpha << isGraphConnected(graph, startVertex);
+
+    return 0;
+}
